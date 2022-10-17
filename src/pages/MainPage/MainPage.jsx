c023ba64e96008af2a6ece52c16f5e0dccab4cb2
moseung <tmdah900@naver.com>
2022-10-17 14:19:26 +0900

    first commit

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainPage = () => {
+  return <Container>메인이에용</Container>;
+};
+
+export default MainPage;
+
+const Container = styled.div``;
---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const colors = {
+  BLACK: "#000000",
+};
+
+const fontSize = {
+  X_SMALL: "12px",
+  SMALL: "14px",
+  MEDIUM: "16px",
+  SEMI_M: "21px",
+  TITLE: "24px",
+};
+
+const theme = {
+  colors,
+  fontSize,
+};
+
+export default theme;
